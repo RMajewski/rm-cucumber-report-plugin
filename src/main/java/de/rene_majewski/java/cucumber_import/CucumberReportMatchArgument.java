c87f52f3331fb3518f048f1b2e9da1324a89b184
René Majewski <rene.majewski@gmx.de>
2022-02-25 10:18:23 +0100

    Added javadocs comments and refactoring.

--- src/main/java/de/rene_majewski/java/cucumber_import/CucumberReportMatchArgument.java
+++ src/main/java/de/rene_majewski/java/cucumber_import/CucumberReportMatchArgument.java
@@ -1,6 +1,67 @@
 package de.rene_majewski.java.cucumber_import;
 
+/**
+ * Speichert die Daten zu einem Argument das im Schritt der auszuführenden
+ * Methode übergeben wird.
+ *
+ * @author René Majewski
+ * @since 0.1.0
+ */
 public class CucumberReportMatchArgument {
-  public String val = null;
-  public int offset = -1;
+  /**
+   * Speichert den Wert des Arguments dass der Methode übergeben wird.
+   */
+  private String val;
+
+  /**
+   * Speichert an welcher Stelle das übergebene Argument in der Zeichenkette
+   * steht, die den Text des Schrittes enthält.
+   */
+  private int offset;
+
+  /**
+   * Setzt die Attribute auf einen definierten Anfangswert.
+   */
+  public CucumberReportMatchArgument() {
+    val = null;
+    offset = -1;
+  }
+
+  /**
+   * Ermittelt den Wert des Arguments.
+   *
+   * @return Wert des Arguments.
+   */
+  public String getVal() {
+    return val;
+  }
+
+  /**
+   * Setzt den Wert des Arguments.
+   *
+   * @param val Wert des Arguments.
+   */
+  public void setVal(String val) {
+    this.val = val;
+  }
+
+  /**
+   * Ermittelt die Stelle in der der Wert in der Zeichenkette mit dem Text des
+   * Schrittes steht.
+   *
+   * @return Stelle an der der Wert des Argumentes beginnt.
+   */
+  public int getOffset() {
+    return offset;
+  }
+
+  /**
+   * Setzt die Stelle in der der Wert in der Zeichenkette mit dem Text des
+   * Schrittes beginnt.
+   *
+   * @param offset Stelle an der der Wert des Argumentes beginnt.
+   */
+  public void setOffset(int offset) {
+    this.offset = offset;
+  }
 }
--- src/main/java/de/rene_majewski/java/cucumber_import/CucumberReportResult.java
+++ src/main/java/de/rene_majewski/java/cucumber_import/CucumberReportResult.java
@@ -1,6 +1,59 @@
 package de.rene_majewski.java.cucumber_import;
 
+/**
+ * Speichert wie lange die Ausführung gedauert hat und welches Ergebnis die
+ * Ausführung hat.
+ *
+ * @author René Majewski
+ * @since 0.1.0
+ */
 public class CucumberReportResult {
-  public long duration = 0;
-  public String status = null;
+  private long duration;
+  private String status;
+
+  /**
+   * Setzt die Attribute auf einen definierten Anfangswert.
+   */
+  public CucumberReportResult() {
+    duration = 0;
+    status = null;
+  }
+
+  /**
+   * Ermittelt die Dauer der Ausführung.
+   *
+   * @return Dauer der Ausführung.
+   */
+  public long getDuration() {
+    return duration;
+  }
+
+  /**
+   * Setzt die Dauer der Ausführung.
+   *
+   * @param duration Dauer der Ausführung.
+   */
+  public void setDuration(long duration) {
+    this.duration = duration;
+  }
+
+  /**
+   * Ermittelt das Ergebnis der Ausführung.
+   *
+   * @return Ergebnis der Ausführung.
+   */
+  public String getStatus() {
+    return status;
+  }
+
+  /**
+   * Setzt das Ergebnis der Ausführung.
+   *
+   * @param status Ergebnis der Ausführung.
+   */
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+
 }
